33464389687a34d89a104ce98aea24040febb8f8
WhatIneedtoDO <zsdfghjkl111@gmail.com>
2023-07-13 11:05:14 +0700

    init

---
+++ src/main/java/com/alibou/security/Controller/AuthRestController.java
@@ -0,0 +1,46 @@
+package com.alibou.security.Controller;
+
+import com.alibou.security.auth.AuthenticationRequest;
+import com.alibou.security.auth.AuthenticationResponse;
+import com.alibou.security.auth.AuthenticationService;
+import com.alibou.security.auth.RegisterRequest;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+//@Controller
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthRestController {
+
+  private final AuthenticationService service;
+//перенести логику в @Controller
+  @PostMapping("/register")
+  public ResponseEntity<AuthenticationResponse> register(
+      @RequestBody RegisterRequest request
+  ) {
+    return ResponseEntity.ok(service.register(request));
+  }
+  @PostMapping("/authenticate")
+  public ResponseEntity<AuthenticationResponse> authenticate(
+      @RequestBody AuthenticationRequest request
+  ) {
+    return ResponseEntity.ok(service.authenticate(request));
+  }
+
+  @PostMapping("/refresh-token")
+  public void refreshToken(
+      HttpServletRequest request,
+      HttpServletResponse response
+  ) throws IOException {
+    service.refreshToken(request, response);
+  }
+
+
+}
