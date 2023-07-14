5e36df07c471ea5bfde61313554b73c86cd3ea4e
ilyaaa <zsdfghjkl111@gmail.com>
2023-07-14 09:22:54 +0700

    чуть чуть поправил

--- src/main/java/com/alibou/security/Controller/AdminController.java
+++ src/main/java/com/alibou/security/Controller/AdminController.java
@@ -1,15 +1,21 @@
 package com.alibou.security.Controller;
 
+import com.alibou.security.auth.AuthenticationResponse;
+import com.alibou.security.auth.AuthenticationService;
+import com.alibou.security.auth.RegisterRequest;
 import io.swagger.v3.oas.annotations.Hidden;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/v1/admin")
 @PreAuthorize("hasRole('ADMIN')")
 public class AdminController {
-
+    private final AuthenticationService service;
     @GetMapping
     @PreAuthorize("hasAuthority('admin:read')")
     public String get() {
